9357c4d49a8a55cfd53c9d0abffede4c19ec518a
Alberto Moreno Monrobe <44670783+xBerto98@users.noreply.github.com>
2019-05-02 19:45:51 +0200

    Update MqttServidor.java

--- MqttServidor.java
+++ MqttServidor.java
@@ -27,14 +27,38 @@ public class MqttServidor extends AbstractVerticle{
 		MqttServer mqttServer = MqttServer.create(vertx);
 		init(mqttServer);
 
+		// Creamos un cliente de prueba para MQTT que publica mensajes cada 3 segundos
 		MqttClient mqttClient = MqttClient.create(vertx, new MqttClientOptions().setAutoKeepAlive(true));
 
+		/*
+		 * Nos conectamos al servidor que está desplegado por el puerto 1883 en la
+		 * propia máquina. Recordad que localhost debe ser sustituido por la IP de
+		 * vuestro servidor. Esta IP puede cambiar cuando os desconectáis de la red, por
+		 * lo que aseguraros siempre antes de lanzar el cliente que la IP es correcta.
+		 */
 		mqttClient.connect(1883, "localhost", s -> {
 
+			/*
+			 * Nos suscribimos al topic_2. Aquí debera indicar el nombre del topic al que os
+			 * queréis suscribir. Además, podéis indicar el QoS, en este caso AT_LEAST_ONCE
+			 * para asegurarnos de que el mensaje llega a su destinatario.
+			 */
 			mqttClient.subscribe("topic_2", MqttQoS.AT_LEAST_ONCE.value(), handler -> {
 				if (handler.succeeded()) {
+					/*
+					 * En este punto el cliente ya está suscrito al servidor, puesto que se ha
+					 * ejecutado la función de handler
+					 */
 					System.out.println("Cliente " + mqttClient.clientId() + " suscrito correctamente al canal topic_2");
 
+					/*
+					 * Además de suscribirnos al servidor, registraremos un manejador para
+					 * interceptar los mensajes que lleguen a nuestro cliente. De manera que el
+					 * proceso sería el siguiente: El cliente externo envía un mensaje al servidor
+					 * -> el servidor lo recibe y busca los clientes suscritos al topic -> el
+					 * servidor reenvía el mensaje a esos clientes -> los clientes (en este caso el
+					 * cliente actual) recibe el mensaje y lo procesa si fuera necesario.
+					 */
 					mqttClient.publishHandler(new Handler<MqttPublishMessage>() {
 						@Override
 						public void handle(MqttPublishMessage arg0) {
@@ -62,21 +86,22 @@ public class MqttServidor extends AbstractVerticle{
 				public void run() {
 					
 					 //Publicamos un mensaje en el topic "topic_2"
-           
-           //A partir de aquí están todos los distintos mensajes que se pueden intercambiar en el canal
-					System.out.println("Introduce tu ID de usuario: ");
-					Integer user=scan.nextInt();
+					//Random random = new Random();
 					System.out.println("Introduce el código correspondiente para realizar la acción que desees: ");
 					Integer code=scan.nextInt();
 					
 					if (mqttClient.isConnected()) {
 						if(code==0){
+						System.out.println("Introduce tu ID de usuario: ");
+						Integer user=scan.nextInt();
 						mqttClient.publish("topic_2",
 								Buffer.buffer(new JsonObject().put("action", "servo_on").put("idUsuario", user)
 										.put("timestamp", Calendar.getInstance().getTimeInMillis())
 										.put("clientId", mqttClient.clientId()).encode()),
 								MqttQoS.AT_LEAST_ONCE, false, false);
 						}else if(code==1){
+						System.out.println("Introduce tu ID de usuario: ");
+						Integer user=scan.nextInt();
 							mqttClient.publish("topic_2",
 									Buffer.buffer(new JsonObject().put("action", "servo_off").put("idUsuario", user)
 											.put("timestamp", Calendar.getInstance().getTimeInMillis())
